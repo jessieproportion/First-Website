92a7fb7fb722b4e95941880aeb6e400a2a12720b
jessieproportion <86574460+jessieproportion@users.noreply.github.com>
2021-06-27 17:04:19 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log(2 + 2)
+
+function showPopup (){
+    var popup = document.getElementById('popup')
+    popup.classList.add('show')
+}
+
+function hidePopup (){
+    var popup = document.getElementById('popup')
+    popup.classList.remove('show')
+}
+
+
+
